910a219d53f3310cf72c18cd53b0575cfda441b0
Alcharif Rami <alchariframi@gmail.com>
2023-11-29 20:55:57 +0200

    update 0.1

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,35 +1,39 @@
-import React from "react";
-import CardItem from "./CardItem";
-import "./Card.css";
+// Cards.js
+import React from 'react';
+import CardItem from './CardItem'; // Adjust the path based on your file structure
+import './Card.css';
 
-function Cards() {
+const Cards = () => {
   return (
     <div className="cards">
       <h1>TEXT</h1>
-      <div className="cards_container">
-        <div className="cards_wrapper">
-          <ul className="cards_items">
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
               src="photos/Flags-picture.jpeg"
               text="Variety of coffee"
               label="Origins"
               path="/origins"
             />
-            
-            
-          </ul>
-          <ul>
-          <CardItem
-              src="photos/Flags-picture.jpeg"
-              text="Variety of coffee"
-              label="Origins"
-              path="/origins"
+            <CardItem
+              src="photos/image_3.jpeg"
+              text="Methods of preparations"
+              label="Brewing"
+              path="/brewing"
             />
-            </ul>
+             <CardItem
+              src="photos/image_4.jpeg"
+              text="How good is the coffee?"
+              label="Rating"
+              path="/ratings"
+            />
+            {/* Add more CardItems as needed */}
+          </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Cards;
